7947cb528b527e5684c53e64103845ff6db13bb5
Ana Margarida <anamargaridarl@gmail.com>
2019-11-24 15:29:22 +0000

    continued to implement navbar

---
+++ navBar.php
@@ -0,0 +1,15 @@
+<nav id="navbar">
+
+<p> 
+<div class="input-container">
+    <i class="fa fa-user icon"></i>
+    <input class="input-field" type="text" placeholder="Local" name="Location">
+  </div>
+
+</p>
+<div id= "profile">
+<img src="images/profile_pic_resize.jpg" alt="Profile Picture">
+<p> Nome </p>
+</div>
+
+</nav>
---
+++ testNav.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Houses Of The World!</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="css/homepage.css" rel="stylesheet">
+  <link href="css/navbar.css" rel="stylesheet">
+
+</head>
+
+<body>
+  <div class="bg">
+  
+      <?php
+        include('navBar.php'); 
+     ?>
+
+    <form id="explore" action="search_rooms.php" method="GET">
+      <h3 id="form_title">Houses of the World!</h3>
+      <div id="whereto">
+        Where<br>
+        <input type="text" name="location">
+      </div>
+      <div id="checkin">
+        Check-In<br>
+        <input type="date" name="checkin">
+      </div>
+      <div id="checkout">
+        Check-Out<br>
+        <input type="date" name="checkout">
+      </div>
+      <div id="guests">
+        Nº guests<br>
+        <input type="number" name="guests">
+      </div>
+      <input id="search" type="submit" value="Search">
+    </form>
+    <section id="signup">
+      <p>Let's go on a adventure</p>
+    </section>
+    <section id="login">
+      <p>Let's get back to business.</p>
+    </section>
+    <footer>
+      <div id="contacts">
+        <a href="contacts.html">Contacts</a>
+      </div>
+      <div>
+        <a href="about_us.html">About Us</a>
+      </div>
+      <div id="atname">
+        <p>@2019 HousesOfTheWorld&trade;</p>
+      </div>
+    </footer>
+  </div>
+</body>
+
+</html>
