e7093dad4e1b33ad0e18edf83265fb764375dbe0
João Martins <up201707311@fe.up.pt>
2019-11-27 22:19:51 +0000

    added signin log in pop modal box

---
+++ modalBox.js
@@ -0,0 +1,26 @@
+const modalSignup = document.querySelector('#modalSignup');
+
+const modalLogin = document.querySelector('#modalLogin');
+
+const sgnup = document.querySelector('#signupbtn');
+
+const login = document.querySelector('#loginbtn');
+
+const explore = document.querySelector('#explore');
+
+sgnup.addEventListener("click", function() {
+  modalSignup.style.display = "block";
+  explore.style.display = "none";
+});
+
+login.addEventListener("click", function() {
+  modalLogin.style.display = "block";
+  explore.style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  if(event.target == modalSignup || event.target == modalLogin) {
+    event.target.style.display = "none";
+    this.explore.style.display = "grid";
+  }
+});
