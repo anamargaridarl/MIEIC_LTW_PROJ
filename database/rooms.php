a838d47e7a03dfbcd1b48fad7064f3b3e75cc309
Goncalo101 <goncalo.oli@outlook.com>
2019-11-27 21:51:01 +0000

    Implemented ability to get rooms based on location

--- database/rooms.php
+++ database/rooms.php
@@ -1,18 +1,17 @@
 <?php
     include_once('database/connection.php');
 
-    function getRooms() {
+    function getRooms($location) {
         global $db;
-        $stmt = $db->prepare('SELECT title, description, price_per_day FROM habitation');
-        $stmt->execute();
+        $stmt = $db->prepare('SELECT title, description, price_per_day FROM habitation WHERE location = ?');
+        $stmt->execute(array($location));
 
         $houses = $stmt->fetchAll();
         
         return $houses;
     }
 
-    function getRoom($id)
-    {
+    function getRoom($id) {
         global $db;
         $stmt = $db->prepare('SELECT title, description, price_per_day FROM habitation WHERE hab_id = ?');
         $stmt->execute(array($id));
@@ -21,6 +20,4 @@
         
         return $houses;
     }
-    
-
 ?>
