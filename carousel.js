d36b143270758eb0694356ec2aff839c8881de7d
João Martins <up201707311@fe.up.pt>
2019-11-29 00:23:55 +0000

    linted JS code

--- carousel.js
+++ carousel.js
@@ -1,32 +1,38 @@
+/* eslint-disable require-jsdoc */
+
 let slideIndex = 1;
 
 // Next/previous controls
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+  showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+  showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
+  let i;
+  const slides = document.getElementsByClassName('mySlides');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex - 1].style.display = 'block';
 }
 
 function update(n) {
-    plusSlides(n);
-    showSlides(n);
+  plusSlides(n);
+  showSlides(n);
 }
 
-document.querySelector(".prev").addEventListener("click", () => update(-1));
-document.querySelector(".next").addEventListener("click", () => update(1));
+document.querySelector('.prev').addEventListener('click', () => update(-1));
+document.querySelector('.next').addEventListener('click', () => update(1));
 
 showSlides(slideIndex);
