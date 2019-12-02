8d486c8a4dd8ada41c1639b22c93e73c30cc5f1b
Ana Margarida <anamargaridarl@gmail.com>
2019-12-02 18:53:48 +0000

    sidemenu in reservation page

--- profile_reservation_page.php
+++ profile_reservation_page.php
@@ -4,10 +4,14 @@
     include_once('templates/footer.php');
     include_once('templates/profile_reservation.php');
     include_once('templates/nav_bar.php');
+    include_once('templates/profile_sidemenu.php');
 
-    drawHead(array("css/profilereservation.css", "css/navfooter.css"), array('modalBox.js'));
+
+    drawHead(array("css/profilereservation.css", "css/profile_sidemenu.css", "css/navfooter.css"), array('modalBox.js'));
 
     drawNavBar();
+
+    drawSideMenu();
 
     $reservations = getReservations(6); 
 
