d45c808e231d9a66edb7f42973914accdef2b438
Goncalo101 <goncalo.oli@outlook.com>
2019-12-05 15:13:12 +0000

    Fixed missing form tag in signup template

--- templates/modalBoxes.php
+++ templates/modalBoxes.php
@@ -3,24 +3,26 @@
 ?>
 
   <section id="modalSignup">
-    <div id="signup">
-      <p id="signTitle">Sign Up</p>
-      <div id="username">
-        <p>Name</p>
-        <input type="text" name="username" placeholder="Enter your Username...">
+    <form method="post" action="../actions/action_signup.php">
+      <div id="signup">
+        <p id="signTitle">Sign Up</p>
+        <div id="username">
+          <p>Name</p>
+          <input type="text" name="username" placeholder="Enter your Username...">
+        </div>
+        <div id="email">
+          <p>Email</p>
+          <input type="text" name="email" placeholder="Enter a valid email...">
+        </div>
+        <div id="password">
+          <p>Password</p>
+          <input type="password" name="password" placeholder="Enter a valid password...">
+        </div>
+        <div id="register">
+          <input type="submit" value="Register">
+        </div>
       </div>
-      <div id="email">
-        <p>Email</p>
-        <input type="text" name="email" placeholder="Enter a valid email...">
-      </div>
-      <div id="password">
-        <p>Password</p>
-        <input type="password" name="password" placeholder="Enter a valid password...">
-      </div>
-      <div id="register">
-        <input type="submit" value="Register">
-      </div>
-    </div>
+    </form>
   </section>
   <section id="modalLogin">
     <form method="post" action="../actions/action_login.php">
