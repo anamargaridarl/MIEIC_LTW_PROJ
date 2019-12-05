b38f06374b19d16df151e62d92d36b1781afa4e9
Goncalo101 <goncalo.oli@outlook.com>
2019-12-05 14:26:11 +0000

    Made the login modal a form

--- templates/modalBoxes.php
+++ templates/modalBoxes.php
@@ -23,19 +23,21 @@
     </div>
   </section>
   <section id="modalLogin">
-    <div id="login">
-      <p id="loginTitle">Sign In</p>
-      <div id="email">
-        <p>Email</p>
-        <input type="text" name="email" placeholder="Enter your email account...">
+    <form method="post" action="../actions/action_login.php">
+      <div id="login">
+        <p id="loginTitle">Sign In</p>
+        <div id="email">
+          <p>Email</p>
+          <input type="text" name="email" placeholder="Enter your email account...">
+        </div>
+        <div id="password">
+          <p>Password</p>
+          <input type="password" name="password" placeholder="Enter your password...">
+        </div>
+        <div id="log">
+          <input type="submit" value="Login">
+        </div>
       </div>
-      <div id="password">
-        <p>Password</p>
-        <input type="password" name="password" placeholder="Enter your password...">
-      </div>
-      <div id="log">
-        <input type="submit" value="Login">
-      </div>
-    </div>
+    </form>
   </section>
 <?php } ?>
