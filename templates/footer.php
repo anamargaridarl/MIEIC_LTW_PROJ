0501bfce598c84a388c9e7eefd7b511237055ada
João Martins <up201707311@fe.up.pt>
2019-12-07 17:00:19 +0000

    fixed footer positioning and page spacing

--- templates/footer.php
+++ templates/footer.php
@@ -1,16 +1,18 @@
 <?php 
   function drawFooter() { ?>
-    <footer>
-      <div id="contacts">
-        <a href="https://www.youtube.com/watch?v=fWNaR-rxAic">Contacts</a>
       </div>
-      <div>
-        <a href="https://www.youtube.com/watch?v=QNJL6nfu__Q">About Us</a>
-      </div>
-      <div id="atname">
-        <p>&copy;2019 HousesOfTheWorld&trade;</p>
-      </div>
-    </footer>
+      <footer>
+        <div id="contacts">
+          <a href="https://www.youtube.com/watch?v=fWNaR-rxAic">Contacts</a>
+        </div>
+        <div>
+          <a href="https://www.youtube.com/watch?v=QNJL6nfu__Q">About Us</a>
+        </div>
+        <div id="atname">
+          <p>&copy;2019 HousesOfTheWorld&trade;</p>
+        </div>
+      </footer>
+    </div>
   </body>
 </html>
 <?php } ?>
