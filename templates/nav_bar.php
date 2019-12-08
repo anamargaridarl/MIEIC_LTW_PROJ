6ae98986d77b308b842277afc6d077c497113f3b
Goncalo101 <goncalo.oli@outlook.com>
2019-12-08 19:52:07 +0000

    Fixed wrong link to profile

--- templates/nav_bar.php
+++ templates/nav_bar.php
@@ -22,7 +22,7 @@
   <?php if(isset($_SESSION['email'])) {?>
     <div id="logged">
       <div><img src="../images/profile_pic.jpg" class="profilepic"></div>
-      <a href="profile.php"><?= $_SESSION['username']?></a>
+      <a href="pages/profile.php"><?= $_SESSION['username']?></a>
       <a href="../actions/action_signout.php" id="signout">Sign Out</a>
     </div>
     <?php } else { ?>
