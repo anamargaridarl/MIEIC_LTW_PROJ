476913034803f80d7eed137a93a31ea1e289d712
João Martins <up201707311@fe.up.pt>
2019-12-13 00:00:26 +0000

    added house add user suggestions and handling of db errors

--- actions/action_addhouse.php
+++ actions/action_addhouse.php
@@ -2,32 +2,25 @@
     include_once('../includes/session.php');
     include_once('../database/houses.php');
 
-if (isset($_POST['title']) && 
-    isset($_POST['region']) && 
-    isset($_POST['location']) && 
-    isset($_POST['address']) &&
-    isset($_POST['capacity']) && 
-    isset($_POST['n_bedrooms']) &&
-    isset($_POST['n_bathrooms']) &&
-    isset($_POST['price']) &&
-    isset($_POST['description']) ){
+    $title = $_POST['title'];
+    $region = $_POST['region'];
+    $location = $_POST['location'];
+    $address = $_POST['address'];
+    $capacity = $_POST['capacity'];
+    $n_rooms = $_POST['n_bedrooms'];
+    $n_bath = $_POST['n_bathrooms'];
+    $price = $_POST['price'];
+    $description = $_POST['description'];
+    try {
+        if(!new_house($title,$region,$location,$address,$capacity,$n_rooms,$n_bath,$price,$description)) {
+            http_response_code(500);
+            die(header('Location: http_error_page.php'));
+        }
+    } catch (PDOException $e) {
+        http_response_code(500);
+        die(header('Location: http_error_page.php'));
+    }
 
-        //add security checks
-        $title = $_POST['title'];
-        $region = $_POST['region'];
-        $location = $_POST['location'];
-        $address = $_POST['address'];
-        $capacity = $_POST['capacity'];
-        $n_rooms = $_POST['n_bedrooms'];
-        $n_bath = $_POST['n_bathrooms'];
-        $price = $_POST['price'];
-        $description = $_POST['description'];
+    header('Location: ../pages/profile_houses_page.php');
 
-        new_house($title,$region,$location,$address,$capacity,$n_rooms,$n_bath,$price,$description);
-
-        header('Location: ../pages/profile_houses_page.php');
-
-  
-}
-  
 ?>
