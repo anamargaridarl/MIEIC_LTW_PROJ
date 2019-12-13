06926c92372cf84c73353628235a28d163f4cc2b
João Martins <up201707311@fe.up.pt>
2019-12-13 03:27:43 +0000

    added some more suggestions on edit houses that were forgotten

--- templates/house_info_edit.php
+++ templates/house_info_edit.php
@@ -8,45 +8,45 @@ $images = getImages($house['hab_id']);?>
     <div id="house_info">
       <div id="h_title">
         <label for="title">Title:</label>
-        <input type="text" name="title" value="<?= htmlspecialchars($house['title'])?>" required>
+        <input type="text" name="title" value="<?= htmlspecialchars($house['title'])?>" title="Give a nice name for your property, you want to catch the attention of all users..." required>
       </div>
       <div id="h_location">
         <div id="region">
           <label for="region">Region:</label>
-          <input type="text" name="region" value="<?= htmlspecialchars($house['region'])?>">
+          <input type="text" name="region" value="<?= htmlspecialchars($house['region'])?>" title="Which region of the country is your place in? In order to appear in most searches you should choose one of these: Norte, Sul, Centro or Islands." required>
         </div>
         <div id="location">
           <label for="location">Location:</label>
-          <input type="text" name="location" value="<?= htmlspecialchars($house['location'])?>" required>
+          <input type="text" name="location" value="<?= htmlspecialchars($house['location'])?>" title="Which city or village is your place in?" required>
         </div>
       </div>
       <div id="h_address">
         <label for="address">Address:</label>
-        <input type="text" name="address" value="<?= htmlspecialchars($house['addr'])?>" nequired>
+        <input type="text" name="address" value="<?= htmlspecialchars($house['addr'])?>" title="Give a more specific address to your place so your guests can find it easier..." nequired>
       </div>
       <div id="h_capacity">
         <div id="capacity">
           <label for="capacity">Capacity:</label>
-          <input type="number" name="capacity" value=<?=$house['capacity']?> required>
+          <input type="number" name="capacity" value=<?=$house['capacity']?> title="How many guests can you accomodate in your place? Should be from 1 to 20..." min="1" max="20" required>
         </div>
         <div id ="n_bathrooms">
           <label for="n_bathrooms">Nº bathrooms:</label>
-          <input type="number" name="n_bathrooms" value=<?=$house['nr_bathrooms']?> required>
+          <input type="number" name="n_bathrooms" value=<?=$house['nr_bathrooms']?> title="How many restrooms does your place have. Remember, this can be a big deal for some customers, so be truthful." required>
         </div>
         <div id="n_bedrooms">
           <label for="n_bedrooms">Nº bedrooms:</label>
-          <input type="number" name="n_bedrooms" value=<?=$house['nr_rooms']?> required>
+          <input type="number" name="n_bedrooms" value=<?=$house['nr_rooms']?> title="How many bedrooms does your place have." required>
         </div>
       </div>
       <div id="h_price">
         <label for="price">Price per day:
-          <input type="number" name="price" value=<?=$house['price_per_day']?> required>
+          <input type="number" name="price" value=<?=$house['price_per_day']?> title="The price you will charge per day. Unless you are not looking to make profit, you should set a price higher than 0..." min="0" required>
         </label>
       </div>
       <div id="h_description">
         <label for="description">Description:</label>
         <div id="h_desc_bot">
-        <textarea name="description" rows="3" cols="80"><?= htmlspecialchars($house['description'])?></textarea>
+        <textarea name="description" rows="3" cols="80" title="Detail your place a little more, what special features does it have: pool, barbecue, jacuzzi, pet friendly, smoking allowed, etc..." required><?= htmlspecialchars($house['description'])?></textarea>
         <div id="save_button">
           <input type="submit" name="save" value="Save">
         </div>
