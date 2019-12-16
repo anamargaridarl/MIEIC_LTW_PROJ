77547fbf9d34db146c1e7df697e76efcac5b665c
João Martins <up201707311@fe.up.pt>
2019-12-16 19:02:41 +0000

    fixed header errors

--- includes/session.php
+++ includes/session.php
@@ -1,6 +1,7 @@
 <?php 
     $currParams = session_get_cookie_params();
     session_set_cookie_params(0,$currParams['path'],$currParams['domain'],false,true);//lifetime is indefinite, secure connection = true (false until https is obtained), httponly = true
+    ob_start();
     session_start();
     if(!isset($_SESSION['csrf'])) {
         $_SESSION['csrf'] = generate_random_token();
