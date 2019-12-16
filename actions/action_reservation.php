8a15a8c99103bdf13418a79e2450e69cc26d07e9
Goncalo101 <goncalo.oli@outlook.com>
2019-12-16 16:41:59 +0000

    Fix reservations

--- actions/action_reservation.php
+++ actions/action_reservation.php
@@ -12,7 +12,13 @@
     $checkin = $_POST['checkin'];
     $checkout = $_POST['checkout'];
 
-    reserve_room($_SESSION['email'], $guests, $hab_id, $checkin, $checkout);
+    if (reserve_room($_SESSION['email'], $guests, $hab_id, $checkin, $checkout)) {
+        $_SESSION['message'] = array('type' => 'success','content' => 'Reservation successful.');
+    } else {
+        $_SESSION['message'] = array('type' => 'error','content' => 'Reservation could not be made.');
+        header("Location: ../pages/reservation.php?id=<?= $hab_id ?>&checkin=<?= $checkin ?>&checkout=<?= $checkout ?>&guests=<?= $guests ?>");
+        die();
+    }
 
     header('Location: ../pages/profile_reservation_page.php');
 ?>
